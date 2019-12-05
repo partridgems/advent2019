07c0a477615ac4034892c23bfe346ae847b9c6dc
Mike Partridge <mikepartridge@google.com>
2019-12-05 00:32:09 -0500

    Finished day 2 (submitted)

--- day2/main.rs
+++ day2/main.rs
@@ -13,39 +13,62 @@ fn stdin_to_array() -> Result<Vec<i32>, Box<dyn Error>> {
 }
 
 fn main() {
-  let mut regs = match stdin_to_array() {
-    Ok(x) => x,
-    Err(e) => {
-      println!("Error getting input: {}", e);
-      return;
-    }
-  };
+    let memory = match stdin_to_array() {
+        Ok(x) => x,
+        Err(e) => {
+            println!("Error getting input: {}", e);
+            return;
+        }
+    };
+    let target = 19690720;
 
-  for pc in (0..regs.len()).step_by(4) {
-    match regs[pc] {
-      1 => {
-    let left_index: usize = regs[pc+1] as usize;
-    let right_index: usize = regs[pc+2] as usize;
-    let target_index: usize = regs[pc+3] as usize;
-        regs[target_index] = regs[left_index] + regs[right_index];
-      }
-      2 => {
-    let left_index: usize = regs[pc+1] as usize;
-    let right_index: usize = regs[pc+2] as usize;
-    let target_index: usize = regs[pc+3] as usize;
-        regs[target_index] = regs[left_index] * regs[right_index];
-      }
-      99 => {
-        println!("Program completed: {}", regs[0]);
-        println!("{:?}", regs);
-        return;
-      }
-      x => {
-        println!("Error, unexpected instruction at location {}: {}", pc, x);
-        println!("{:?}", regs);
-        return;
-      }
+    // Noun/verb for the program
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let ret_val: i32 = run_program(&memory, noun, verb);
+            if ret_val == target {
+                println!("Found target. Noun: {}, Verb: {}", noun, verb);
+                return;
+            }
+        }
     }
-  }
-  println!("Error, reached end of program with no termination");
+}
+
+fn run_program(memory: &Vec<i32>, noun: i32, verb: i32) -> i32 {
+    // Copy and adjust mem for this run
+    let mut regs: Vec<i32> = memory.to_vec();
+    regs[1] = noun;
+    regs[2] = verb;
+    let mut pc: usize = 0;
+    loop {
+        if pc >= regs.len() {
+            println!("Error, reached end of memory without terminating.");
+            return -1;
+        }
+        match regs[pc] {
+        1 => {
+            let left_index: usize = regs[pc+1] as usize;
+            let right_index: usize = regs[pc+2] as usize;
+            let target_index: usize = regs[pc+3] as usize;
+            regs[target_index] = regs[left_index] + regs[right_index];
+            pc += 4;
+        }
+        2 => {
+            let left_index: usize = regs[pc+1] as usize;
+            let right_index: usize = regs[pc+2] as usize;
+            let target_index: usize = regs[pc+3] as usize;
+            regs[target_index] = regs[left_index] * regs[right_index];
+            pc += 4;
+        }
+        99 => {
+//            pc += 1;
+            return regs[0];
+        }
+        x => {
+            println!("Error, unexpected instruction at location {}: {}", pc, x);
+            println!("{:?}", regs);
+            return -1;
+        }
+    }
+}
 }
